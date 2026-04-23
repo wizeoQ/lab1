e1885da05f0171ca183d2c626270b00ea136d433
wizeoQ <quartetqoc@gmail.com>
2026-04-23 19:05:27 +0300

    Создан файл Л.Р. №1

---
+++ lab1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 18:52:33 2026
+
+@author: wizeo
+"""
+
+name = input ("Ваши имя, фамилия, отчество?\t> ")
+age = input ("Сколько Вам лет?\t> ")
+place = input ("Где Вы живёте?\t> ")
+
+print("Ваши ФИО -", name, end="\n")
+print("Ваш возраст -", age, end="\n")
+print("Вы живёте в -", place, end="")
