0993b91bd27d81ab329202f01c49e4812d1b71fe
wizeoQ <quartetqoc@gmail.com>
2026-04-23 19:24:54 +0300

    Добавлена функция для быстрого создания красивых разделений в строке, а также были проведены некоторые косметические улучшения

--- lab1.py
+++ lab1.py
@@ -5,11 +5,22 @@ Created on Thu Apr 23 18:52:33 2026
 
 @author: wizeo
 """
+def D(n):
+    if n == 1:
+        print("\n-----\n")
+    elif n == 2:
+        print("\n=========\n")
+    else:
+        print('\n---\n')
 
-name = input ("Ваши имя, фамилия, отчество?\t> ")
-age = input ("Сколько Вам лет?\t> ")
-place = input ("Где Вы живёте?\t> ")
+name = input ("Ваши имя, фамилия, отчество?\n\t> ")
+age = input ("Сколько Вам лет?\n\t> ")
+place = input ("Где Вы живёте?\n\t> ")
+
+D(1)
 
 print("Ваши ФИО -", name, end="\n")
 print("Ваш возраст -", age, end="\n")
 print("Вы живёте в -", place, end="")
+
+D(2)
