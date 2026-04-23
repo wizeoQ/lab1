0d741d02fbeb10de2abe39ac506bb7727f548de1
wizeoQ <quartetqoc@gmail.com>
2026-04-23 20:33:12 +0300

    Разделения теперь являются классом, а не функцией

--- lab1.py
+++ lab1.py
@@ -5,22 +5,35 @@ Created on Thu Apr 23 18:52:33 2026
 
 @author: wizeo
 """
-def D(n):
-    if n == 1:
-        print("\n-----\n")
-    elif n == 2:
-        print("\n=========\n")
-    else:
-        print('\n---\n')
+class r():
+    def z(number):
+        print("\n==========")
+        print("Задание ", number)
+    def t(tire=5):
+        i=0
+        print('')
+        while i!=tire:
+            print("-", end='')
+            i+=1
+        print('')
+    def r(ravno=5):
+        i=0
+        print('')
+        while i!=ravno:
+            print("=", end='')
+            i+=1
+        print('')
+
+r.z(1)
 
 name = input ("Ваши имя, фамилия, отчество?\n\t> ")
 age = input ("Сколько Вам лет?\n\t> ")
 place = input ("Где Вы живёте?\n\t> ")
 
-D(1)
+r.t(3)
 
 print("Ваши ФИО -", name, end="\n")
 print("Ваш возраст -", age, end="\n")
 print("Вы живёте в -", place, end="")
 
-D(2)
+r.r()
