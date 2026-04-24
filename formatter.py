73c150b76c3d19134f5268075b527f9638f29d09
wizeoQ <quartetqoc@gmail.com>
2026-04-24 08:45:31 +0300

    В основном файле класс заменён на библиотеку formatter.

--- formatter.py
+++ formatter.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 
 def z(number):
-        print("\n==========")
-        print("Задание ", number)
+    print("\n==========")
+    print("Задание ", number)
 def t(tire=5):
-        i=0
-        print('')
-        while i!=tire:
-            print("-", end='')
-            i+=1
-        print('')
+    i=0
+    print('')
+    while i!=tire:
+        print("-", end='')
+        i+=1
+    print('')
 def r(ravno=5):
-        i=0
-        print('')
-        while i!=ravno:
-            print("=", end='')
-            i+=1
-        print('')
+    i=0
+    print('')
+    while i!=ravno:
+        print("=", end='')
+        i+=1
+    print('')
--- lab1.py
+++ lab1.py
@@ -1,39 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 23 18:52:33 2026
 
-@author: wizeo
-"""
-class r():
-    def z(number):
-        print("\n==========")
-        print("Задание ", number)
-    def t(tire=5):
-        i=0
-        print('')
-        while i!=tire:
-            print("-", end='')
-            i+=1
-        print('')
-    def r(ravno=5):
-        i=0
-        print('')
-        while i!=ravno:
-            print("=", end='')
-            i+=1
-        print('')
+import formatter as f
 
-r.z(1)
+f.z(1)
 
 name = input ("Ваши имя, фамилия, отчество?\n\t> ")
 age = input ("Сколько Вам лет?\n\t> ")
 place = input ("Где Вы живёте?\n\t> ")
 
-r.t(3)
+f.t(3)
 
 print("Ваши ФИО -", name, end="\n")
 print("Ваш возраст -", age, end="\n")
 print("Вы живёте в -", place, end="")
 
-r.r()
+f.r()
