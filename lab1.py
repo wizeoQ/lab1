af063b7abeeec64dc78c07ad2e1437575331a105
wizeoQ <quartetqoc@gmail.com>
2026-04-24 10:55:18 +0300

    t принимает значения от 0 до 10 с шагом 1

--- lab1.py
+++ lab1.py
@@ -4,17 +4,17 @@
 import formatter as f
 import numpy as n
 
-f.ex(1)
-
-name = input ("Ваши имя, фамилия, отчество?\n\t> ")
-age = input ("Сколько Вам лет?\n\t> ")
-place = input ("Где Вы живёте?\n\t> ")
-
-f.d()
-
-print("Ваши ФИО -", name, end="\n")
-print("Ваш возраст -", age, end="\n")
-print("Вы живёте в -", place, end="")
+#f.ex(1)
+#
+#name = input ("Ваши имя, фамилия, отчество?\n\t> ")
+#age = input ("Сколько Вам лет?\n\t> ")
+#place = input ("Где Вы живёте?\n\t> ")
+#
+#f.d()
+#
+#print("Ваши ФИО -", name, end="\n")
+#print("Ваш возраст -", age, end="\n")
+#print("Вы живёте в -", place, end="")
 
 f.ex(2)
 
@@ -25,10 +25,15 @@ print("    sqrt(t)-|sin(t)|")
 f.l(5)
 
 x=float(input("Введите x - "))
-t=float(input("Введите t - "))
-
-a=9*n.pi*t+10*n.cos(x)
-b=n.sqrt(t)-abs(n.sin(t))
-z=a/b*n.e**x
-
-print("\nОтвет: Z =", round(z,2))
+#t=[1,2,3,4,5,6,7,8,9,10]
+t=0.0
+while t!=11:
+    if t==0:
+        z=0
+        print("Z("+str(int(t))+") =", round(z,2))
+    else:
+        a=9*n.pi*t+10*n.cos(x)
+        b=n.sqrt(t)-abs(n.sin(t))
+        z=a/b*n.e**x
+        print("Z("+str(int(t))+") =", round(z,2))
+    t+=1
