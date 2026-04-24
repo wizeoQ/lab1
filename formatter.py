f9038f176833f6f0621694fca0379547424e718b
wizeoQ <quartetqoc@gmail.com>
2026-04-24 10:01:59 +0300

    В библиотеку formatter добавлена функция l, вставляющая линию в середине.

--- formatter.py
+++ formatter.py
@@ -4,11 +4,13 @@
 def ex(exercize):
     print("\n==========")
     print("Задание ", exercize)
-def d(dash=5):
-    print('\n'+"-"*dash)
 def eq(equal=5):
     print('\n'+"="*equal)
+def d(dash=5):
+    print('\n'+"-"*dash)
 def n(nice=None):
     print("\n༺•༻")
+def l(line=5):
+    print('─'*line)
 def ln(long_nice=None):
     print("\n───────✧───────")
