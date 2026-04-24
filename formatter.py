faf7e0f39c74446bab9e093a3631997075c44d91
wizeoQ <quartetqoc@gmail.com>
2026-04-24 08:27:04 +0300

    Создана библиотека formatter, которая создаёт разделители строк

---
+++ formatter.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def z(number):
+        print("\n==========")
+        print("Задание ", number)
+def t(tire=5):
+        i=0
+        print('')
+        while i!=tire:
+            print("-", end='')
+            i+=1
+        print('')
+def r(ravno=5):
+        i=0
+        print('')
+        while i!=ravno:
+            print("=", end='')
+            i+=1
+        print('')
