e2bc9e1819aa27c2846fa42b19d3a29ad5d25531
wizeoQ <quartetqoc@gmail.com>
2026-04-25 15:57:25 +0300

    Код оптимизирован, улучшен вывод графика, добавлен вывод мнимой части z, везде добавлена проверка типа данных, программа больше не падает отстранных переменных, в случае деления на ноль теперь результат равен nan. Часть кода приведена к общепринятым стандартам.

--- ex2.py
+++ ex2.py
@@ -11,66 +11,97 @@ def run_exercise_2():
     print("    sqrt(t)-|sin(t)|")
     f.l(5)
     #-------------Ввод данных----------------    
-    x=float(input("Введите x > "))
+    while True:
+        try:
+            x=float(input("Введите x > "))
+            break
+        except ValueError:
+            print("Неверный тип данных.")
     print("Введите t")
     print("Если хотите ввести массив - вводите через запятую")
     print("Если нужен диапазон, введите \"d\"",end="")
-    t=str(input("t > "))
+    t=""
+    while True:
+        try:
+            t=str(input("t > "))
+            if t=='d':
+                break
+            #-----Проверка t    
+            t=t.replace(' ','')
+            t=t.split(',')
+            t=list(map(float,t))
+            elements_output='Y'
+            break
+        except ValueError:
+            print("Неверный тип данных.")
     f.l(5)
     #----------Построение диапазона----------
-    t_list=[]
+    '''linspace используется трижды как индикатор того,
+    что пользователь ввёл правильнй тип данных (т.к.
+    для points помимо значения float нужно также >=0'''
+    
     if t=='d':
-        start = float(input("Введите старт массива > "))
-        end = float(input("Введите конец массива > "))
-        points = int(input("Введите количество элементов > "))
-        elements_output = input("Нужен ли вывод ответов? Их может быть много. Y/N > ")
-        if points<=0:
-            print("Так не бывает.")
-            t=[]
-        elif points==1:
-            t=np.linspace(start,start,1)
-        else:
-            t=np.linspace(start,end,points)
-        t_list=t
-    #-----Перевод строки в лист вещественных значений--
-    elif t!='d':
-        t=t.replace(' ','')
-        t_list=t.split(',')
-        l = len(t_list)
-        t_list_temp=[]
-        for i in t_list:
-            i=float(i)
-            t_list_temp.append(i)
-        t_list=t_list_temp
-        elements_output='Y'
+        while True:
+            try:
+                start = float(input("Введите старт массива > "))
+                t=np.linspace(start,0,1)
+                break
+            except ValueError:
+                print("Неверный тип данных.")
+        while True:
+            try:
+                end = float(input("Введите конец массива > "))
+                t=np.linspace(start,end,1)
+                break
+            except ValueError:
+                print("Неверный тип данных.")
+        while True:
+            try:
+                points = int(input("Введите количество элементов > "))
+                t=np.linspace(start,end,points)
+                break
+            except ValueError:
+                print("Неверный тип данных")
+                print("или число точек отрицательное.")
+        #-------Нужен ли вывод значений-------
+        print("Нужен ли вывод ответов?")
+        print("Предупреждение: Их может быть много.")
+        elements_output = input("Y/N > ")
+        #=------------------------------------
+        if points==0:
+            print("Элементы отсутствуют.")
+            t=[]        
     #------------------Блок вычислений----------------
-    z_list=[]
+    z=[]
     i=0
-    while i!=len(t_list):
-        t = float(t_list[i])
-        if t==0:
-            z=0
+    while i!=len(t):
+        if t[i]==0:
+            ans=np.nan
             if elements_output=='Y' or elements_output=='y':
-                print("Z("+str(round(t,2))+") =", round(z,2))
+                print("Z(0.0) = nan")
         else:
-            a=9*np.pi*t+10*np.cos(x)
-            b=np.sqrt(complex(t))-abs(np.sin(t))
-            z=a/b*np.exp(x)
+            a=9*np.pi*t[i]+10*np.cos(x)
+            b=np.sqrt(complex(t [i]))-abs(np.sin(t[i]))
+            ans=a/b*np.exp(x) #ans - float значение внутри z
             if elements_output=='Y' or elements_output=='y':
-                if np.imag(z)<0:
-                    print("Z("+str(round(t,2))+") =", str(np.round(np.real(z),2))+'-'+str(np.round(np.imag(z),2))+'i')    
-                elif np.imag(z)>0:
-                    print("Z("+str(round(t,2))+") =", str(np.round(np.real(z),2))+'+'+str(np.round(np.imag(z),2))+'i')
+                if np.imag(ans)<0:
+                    print("Z("+str(round(t[i],2))+") =", str(np.round(np.real(ans),2))+str(np.round(np.imag(ans),2))+'i')    
+                elif np.imag(ans)>0:
+                    print("Z("+str(round(t[i],2))+") =", str(np.round(np.real(ans),2))+'+'+str(np.round(np.imag(ans),2))+'i')
                 else:
-                    print("Z("+str(round(t,2))+") =", str(np.round(z,2)))
-        z_list.append(np.round(z,2))
+                    print("Z("+str(round(t[i],2))+") =", str(np.real(np.round(ans,2))))
+        z.append(ans)
         i+=1
     #------------------Вывод графика----------------
-    choise=input("Вывести график? Y/N > ")
-    if choise=='y':
-        plt.plot(t_list,np.real(z_list))
+    choice=input("Вывести график? \nY/N > ")
+    if choice=='y' or choice=='Y':
+        plt.plot(np.round(t,2),np.round(np.real(z),2),'|r-')
+        plt.plot(np.round(t,2),np.round(np.imag(z),2),'|b:')
+        plt.legend(['Re(Z)','Im(Z)'])
+        plt.title("Зависимость Z от t")
         plt.xlabel("t")
         plt.ylabel("Z(t)")
+        plt.grid()
         plt.show()
     else:
         pass
