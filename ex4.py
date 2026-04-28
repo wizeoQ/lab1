14bf2f0e1dabdb4d5d04472c8453ec1d6b3d1992
wizeoQ <quartetqoc@gmail.com>
2026-04-28 10:25:59 +0300

    Вместо чёткой цены теперь рандом, добавлено маркетинговое преобразование в случае круглой суммы, код оптимизирован

--- ex4.py
+++ ex4.py
@@ -1,9 +1,11 @@
 import formatter as f
-
+import random as rnd
 f.ex(4.1)
 
-price=299.99
+price=float(rnd.randint(50,1000))
+if price%100==0:price-=0.01
 for i in range(1,11):
     print(i,' кг конфет - ', round(i*price,2), ' рублей.',sep='')
+del i,price
 
 f.eq()
