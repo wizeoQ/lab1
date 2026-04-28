2528f2e99e92d787ae1ca59acb11b7ecfe664794
wizeoQ <quartetqoc@gmail.com>
2026-04-28 17:13:45 +0300

    Добавлена функция расчёта выражения из упражнения 6

--- ex6.py
+++ ex6.py
@@ -24,5 +24,40 @@ while True:
         if 0.1<=a<=5: break
         else:print("Значения нет в диапазоне.")
     except ValueError: print("Неверный тип данных")
-    
 f.l()
+#Крайние случаи
+if len(list(t))==1 and t[0]==0:print("Деление на ноль.")
+#Расчёт выражения.
+else:
+    z=[]
+    for t_i in t:
+        if t_i==0:z.append(np.nan)
+        else:
+            nmrtr=a*((5*np.pi*(t_i**2))+3*np.cos(2*x))
+            dnmtr=np.log(abs(t_i)+1)-np.sin(t_i)**2
+            answ=nmrtr/dnmtr*np.exp(-a*x)+np.sqrt(complex(a*t_i))
+            z.append(answ)
+    #Вывод данных.
+    print("Вывести данные? Y/N",end='')
+    while True:
+        out_flag=input('>')
+        f.l()
+        if out_flag=="":continue
+        if out_flag in "yY1дД":
+            print("Z(t, x, a)= ...")
+            for t_i,z_i in zip(t,z):
+                if float(a)==int(a):a=int(a) #константа a
+                if float(x)==int(x):x=int(x) #константа x
+                if float(t_i)==int(t_i):t_i=int(t_i) #убираем лишний .0
+                if t_i==0:z_ir,z_ii="nan",'' #случай nan
+                else:
+                    z_ir=round(float(np.real(z_i)),2)
+                    z_ii=round(float(np.imag(z_i)),2)
+                    if float(z_ir)==int(z_ir):z_ir=int(z_ir) #убираем лишний .0
+                    if float(z_ii)==int(z_ii):z_ii=int(z_ii) #убираем лишний .0
+                    if z_ii==0:z_ii='' #число не мнимое
+                    elif z_ii>0:z_ii='+'+str(z_ii)+'i'
+                    elif z_ii<0:z_ii=str(z_ii)+'i' #минус сам допишется
+                print("Z(",t_i,", ",x,", ",a,") = ",z_ir,z_ii,sep='')
+            break
+        if out_flag in "nN0нН":break
