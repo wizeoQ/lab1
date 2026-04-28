81b8be0569a84b4cdcfa7c28f059f72efab88323
wizeoQ <quartetqoc@gmail.com>
2026-04-28 14:15:40 +0300

    Добавлен ввод данных с проверкой на принадлежность диапазону и проверкой на корректность типа данных.

--- ex6.py
+++ ex6.py
@@ -1,7 +1,27 @@
 import formatter as f
+import numpy as np
+import matplotlib.pyplot as plt
 
 f.ex(6)
 
-#67
+#Ввод данных
+while True:
+    try:
+        t=float(input("Введите t\n>"))
+        break
+    except ValueError:print("Неверный тип данных")
+
+while True:
+    try:
+        x=float(input("Введите x\n>"))
+        break
+    except ValueError:print("Неверный тип данных")
+
+while True:
+    try:
+        a=float(input("Введите a (диапазон 0.1 до 5.0)\n>"))
+        if 0.1<=a<=5: break
+        else:print("Значения нет в диапазоне.")
+    except ValueError: print("Неверный тип данных")
 
 f.eq()
