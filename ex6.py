3b5ffb188e4b930ad3e40091b29ca28d7c665499
wizeoQ <quartetqoc@gmail.com>
2026-04-28 22:41:41 +0300

    Доделано упражнение 6 - функция доведена до ума, строится график, интерактивные вопросы пользователю.

--- ex6.py
+++ ex6.py
@@ -2,101 +2,103 @@ import formatter as f
 import numpy as np
 import matplotlib.pyplot as plt
 
-f.ex(6)
-
-#Вывод формулы
-print("\n          / a*[(5pi*t)^2+3cos(2x)] \\")
-print("Z(t,x,a)= |────────────────────────| * beta")
-print("          \\    ln(|t|+1)-sin(t)^2  /")
-print("\n, где beta - e^(-ax)+sqrt(at)")
-f.l()
-#Ввод данных
-print("Введите t\nДля ввода массива используйте запятые.",end="")
-print("Для ввода диапазона введите d.",end="")
-t=input(">")
-
-#Ввод диапазона
-if t!='d':
-    try:
-        t=list(map(float,(t.split(','))))
-    except ValueError:
-        print("\nНеверный тип данных\nВведите t",end="")
+def run_exercise_6():
+    f.ex(6)
+    
+    #Вывод формулы
+    print("\n          / a*[(5pi*t)^2+3cos(2x)] \\")
+    print("Z(t,x,a)= |────────────────────────| * beta")
+    print("          \\    ln(|t|+1)-sin(t)^2  /")
+    print("\n, где beta - e^(-ax)+sqrt(at)")
+    f.l()
+    #Ввод данных
+    print("Введите t\nДля ввода массива используйте запятые.",end="")
+    print("Для ввода диапазона введите d.",end="")
+    t=input(">")
+    
+    #Ввод диапазона
+    if t!='d':
+        try:
+            t=list(map(float,(t.split(','))))
+        except ValueError:
+            print("\nНеверный тип данных\nВведите t",end="")
+            while True:
+                try:
+                    t=input('>')
+                    if t=='d':break
+                    t=list(map(float,t.split(',')))
+                    break
+                except ValueError:print("Неверный тип данных")
+        points=1
+    #Ввод t
+    if t=='d':
+        t=0
         while True:
             try:
-                t=input('>')
-                if t=='d':break
-                t=list(map(float,t.split(',')))
+                start=float(input("Введите начало массива\n>"))
+                break
+            except ValueError:print("Неверный тип данных\n>")
+        while True:
+            try:
+                end=float(input("Введите конец массива\n>"))
+                break
+            except ValueError:print("Неверный тип данных.")
+        while True:
+            try:
+                points=int(input("Введите количество элементов\n>"))
+                t=np.linspace(start,end,points)
+                break
+            except (ValueError, TypeError):print("Неверный тип данных или отрицательное значение.")
+    
+    #Крайние случаи       
+    if len(list(t))==1 and t[0]==0:print("Деление на ноль.")
+    elif points==0:print("Значения отсутствуют.")
+    
+    #Расчёт выражения.
+    else:
+        while True:
+            try:
+                x=float(input("Введите x\n>"))
                 break
             except ValueError:print("Неверный тип данных")
-    points=1
-#Ввод t
-if t=='d':
-    t=0
-    while True:
-        try:
-            start=float(input("Введите начало массива\n>"))
-            break
-        except ValueError:print("Неверный тип данных\n>")
-    while True:
-        try:
-            end=float(input("Введите конец массива\n>"))
-            break
-        except ValueError:print("Неверный тип данных.")
-    while True:
-        try:
-            points=int(input("Введите количество элементов\n>"))
-            t=np.linspace(start,end,points)
-            break
-        except (ValueError, TypeError):print("Неверный тип данных или отрицательное значение.")
-
-#Крайние случаи       
-if len(list(t))==1 and t[0]==0:print("Деление на ноль.")
-elif points==0:print("Значения отсутствуют.")
-
-#Расчёт выражения.
-else:
-    while True:
-        try:
-            x=float(input("Введите x\n>"))
-            break
-        except ValueError:print("Неверный тип данных")
-
-    while True:
-        try:
-            a=float(input("Введите a (диапазон 0.1 до 5.0)\n>"))
-            if 0.1<=a<=5: break
-            else:print("Значения нет в диапазоне.")
-        except ValueError: print("Неверный тип данных")
-    f.l()
-    z=[]
-    for t_i in t:
-        if t_i==0:z.append(np.nan)
-        else:
-            nmrtr=a*((5*np.pi*(t_i**2))+3*np.cos(2*x))
-            dnmtr=np.log(abs(t_i)+1)-np.sin(t_i)**2
-            answ=nmrtr/dnmtr*np.exp(-a*x)+np.sqrt(complex(a*t_i))
-            z.append(answ)
-    #Вывод данных.
-    print("Вывести данные? Y/N",end='')
-    while True:
-        out_flag=input('>')
+    
+        while True:
+            try:
+                a=float(input("Введите a (диапазон 0.1 до 5.0)\n>"))
+                if 0.1<=a<=5: break
+                else:print("Значения нет в диапазоне.")
+            except ValueError: print("Неверный тип данных")
         f.l()
-        if out_flag=="":continue
-        if out_flag in "yY1дД":
+        z=[]
+        for t_i in t:
+            if t_i==0:z.append(np.nan)
+            else:
+                nmrtr=a*((5*np.pi*(t_i**2))+3*np.cos(2*x))
+                dnmtr=np.log(abs(t_i)+1)-np.sin(t_i)**2
+                answ=nmrtr/dnmtr*np.exp(-a*x)+np.sqrt(complex(a*t_i))
+                z.append(answ)
+        #Вывод данных.
+        def out_z():
+            f.l()
             print("Z(t, x, a)= ...")
+            x_out=f.simple_complex(x,3,0)
+            a_out=f.simple_complex(a,3,0)
             for t_i,z_i in zip(t,z):
-                if float(a)==int(a):a=int(a) #константа a
-                if float(x)==int(x):x=int(x) #константа x
-                if float(t_i)==int(t_i):t_i=int(t_i) #убираем лишний .0
-                if t_i==0:z_ir,z_ii="nan",'' #случай nan
-                else:
-                    z_ir=round(float(np.real(z_i)),3)
-                    z_ii=round(float(np.imag(z_i)),3)
-                    if float(z_ir)==int(z_ir):z_ir=int(z_ir) #убираем лишний .0
-                    if float(z_ii)==int(z_ii):z_ii=int(z_ii) #убираем лишний .0
-                    if z_ii==0:z_ii='' #число не мнимое
-                    elif z_ii>0:z_ii='+'+str(z_ii)+'i'
-                    elif z_ii<0:z_ii=str(z_ii)+'i' #минус сам допишется
-                print("Z(",round(t_i,3),", ",x,", ",a,") = ",z_ir,z_ii,sep='')
-            break
-        if out_flag in "nN0нН":break
+                t_out=f.simple_complex(t_i,3,0)
+                z_out=f.simple_complex(z_i,3,0)
+                print("Z(",t_out,", ",x_out,", ",a_out,") = ",z_out,sep='')
+        print("Вывести данные? Y/N",end='')
+        f.yes_or_not(out_z)
+        #Построение графика
+        def graph():
+            plt.plot(t,np.real(z),'-r')
+            plt.plot(t,np.imag(z),'-g')
+            plt.xlabel("t")
+            plt.ylabel("Z(t)")
+            plt.title("Зависимость Z от t")
+            plt.legend(["Re(Z)","Im(Z)"])
+            plt.show()
+        print("Вывести график? Y/N",end='')
+        f.yes_or_not(graph)
+        
+run_exercise_6()
